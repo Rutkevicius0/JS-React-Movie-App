db7b216334fd4d90546da6f7a2f97be8ba2aeeaa
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-06-09 13:42:27 +0300

    movie table done

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -4,23 +4,54 @@ import { getMovies } from '../services/fakeMovieService';
 class MovieTable extends Component {
   state = {
     movies: getMovies(),
-    count: 1,
   };
   render() {
     return (
       <div>
-        <h3>movie table is here</h3>
-        <ol>
-          {this.state.movies.map((m) => (
-            <li key={m._id}>
-              <p>{m.title}</p>
-              <span>IMDB</span>
-            </li>
-          ))}
-        </ol>
+        <h3>Movie suggestions</h3>
+        <p> xx movies in our rental store</p>
+
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rating</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((m) => (
+              <tr key={m._id}>
+                <td>{m.title}</td>
+                <td>{m.genre.name}</td>
+                <td>{m.numberInStock}</td>
+                <td>{m.dailyRentalRate}</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
+  //   createTable() {
+  //     let table = document.getElementsByTagName('table');
+  //     let tableBody = document.getElementsByTagName('tbody');
+  //     this.state.movies.map((rowData) => {
+  //       let row = document.createElement('tr');
+  //       rowData.map((cellData) => {
+  //         console.log(cellData);
+  //         let cell = document.createElement('td');
+  //         cell.appendChild(document.createTextNode(cellData));
+  //         row.appendChild(cell);
+  //       });
+  //       tableBody.appendChild(row);
+  //     });
+  //   }
 }
 
 export default MovieTable;
