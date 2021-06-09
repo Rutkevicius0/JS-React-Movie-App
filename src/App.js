f472a3b1bcaf0d9868dc086f7152927659f60117
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-06-09 11:29:32 +0300

    MovieTable component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MovieTable from './components/movieTable';
 
 function App() {
   return (
     <div className="App">
       <h1>yes</h1>
+      <MovieTable />
     </div>
   );
 }
---
+++ src/components/movieTable.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MovieTable extends Component {
+  state = {};
+  render() {
+    return <h3>movie table is here</h3>;
+  }
+}
+
+export default MovieTable;
